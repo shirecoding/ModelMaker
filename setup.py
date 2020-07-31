31f274e08479f5c4ba0b4c531528d52318cfbde1
Benjamin Hon <benjaminhon86@gmail.com>
2020-07-31 15:16:26 +0800

    added new project command

--- setup.py
+++ setup.py
@@ -6,10 +6,21 @@ setup(
     name='modelmaker',
     version=__version__,
     author='shirecoding',
-    packages=find_packages(),
+    scripts=['bin/modelmaker'],
     install_requires=[],
     url='https://github.com/shirecoding/ModelMaker',
     long_description=open('README.md').read(),
     zip_safe=False,
-    scripts=['bin/modelmaker']
+    include_package_data=True,
+    packages=find_packages() + [
+        'modelmaker.resources'
+    ],
+    package_data={
+        'modelmaker.resources': [
+            'project_template/*',
+            'project_template/**/*',
+            'templates/*',
+            'templates/**/*',
+        ]
+    }
 )
