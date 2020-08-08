f7ff37daf404a8a4e230622fadb40ea47e0efa46
Benjamin Hon <benjaminhon86@gmail.com>
2020-08-08 15:58:18 +0800

    fix packaging

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import find_packages
 from modelmaker import __version__
 
 setup(
-    name='modelmaker',
+    name='model-maker',
     version=__version__,
     author='shirecoding',
+    author_email="shirecoding@gmail.com",
     scripts=['bin/modelmaker'],
     install_requires=[
         'Jinja2',
@@ -15,8 +16,12 @@ setup(
     url='https://github.com/shirecoding/ModelMaker',
     download_url='https://github.com/shirecoding/ModelMaker/archive/0.0.1.tar.gz',
     long_description=open('README.md').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     zip_safe=False,
-    include_package_data=True,
     packages=find_packages() + [
         'modelmaker.resources'
     ],
